21819b144aa6a7935b000df15bd5aeec03711fd8
Boyan Hristov <bah0046@uah.edu>
2024-08-22 20:16:55 -0500

    Alert schema save work

---
+++ gcn/notices/fermi/gbm/test/validate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import os.path
+from sys import argv
+#from urllib.parse import urlsplit
+import json
+from jsonschema import validate, Draft202012Validator
+from referencing import Registry, Resource
+import unittest
+
+def RETRIEVE_LOCAL_FILE(uri):
+   #parsed = urlsplit(uri)
+   u = uri.replace("https://gcn.nasa.gov/schema/main", "../../../../..")
+   print(uri, "-->", u)
+   with open(u) as f:
+      s = f.read()
+
+   s = json.loads(s)
+   s = Resource.from_contents(s)
+   return s
+
+REGISTRY = Registry(retrieve=RETRIEVE_LOCAL_FILE)
+
+def test_base(self, instance):
+   def test_1(self):
+      pass
+
+   pass
+
+class TestAlert(unittest.TestCase):
+   def setUp(self):
+      my_schema = "../alert.schema.json"
+      my_schema = os.path.abspath(my_schema)
+
+      with open(my_schema) as s:
+         my_schema = s.read()
+      my_schema = json.loads(my_schema)
+
+      self.validator = Draft202012Validator(schema=my_schema, registry=REGISTRY)
+
+   def test1(self):
+      my_json = json.loads('{"TRIGGER_ALGORITHM": 15}')
+      assertTrue(v.is_valid(my_json))
+
+
+if __name__ == "__main__":
+   my_schema = "../alert.schema.json"
+   my_schema = os.path.abspath(my_schema)
+
+   with open(my_schema) as s:
+      my_schema = s.read()
+   my_schema = json.loads(my_schema)
+   print(my_schema)
+
+   v = Draft202012Validator(schema=my_schema, registry=REGISTRY)
+
+   #my_json = json.loads('{"filter": ["Hello world!"]}')
+   my_json = json.loads('{"TRIGGER_ALGORITHM_NUMBER": 15, "mission":"AAAA"}')
+   v.validate(instance=my_json)
+   print("Valid")
